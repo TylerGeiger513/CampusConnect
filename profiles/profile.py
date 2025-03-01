c1ce42b9dc15e917abbc4ee83d1e258b3cc0cebb
Tyler <tygg513@outlook.com>
2025-03-01 11:14:00 -0500

    update profile

--- profiles/profile.py
+++ profiles/profile.py
@@ -4,10 +4,11 @@ import geni.rspec.pg as rspec
 # Create a request object
 request = portal.context.makeRequestRSpec()
 
+
 # Define the primary node for Kubernetes and Docker
 k8s_node = request.RawPC("k8s-master")
 k8s_node.hardware_type = "m400"
-k8s_node.disk_image = "urn:publicid:IDN+apt.emulab.net+authority+cm:UBUNTU22-64-STD"
+k8s_node.disk_image = "urn:publicid:IDN+emulab.net+authority+cm:UBUNTU22-64-STD"
 
 # Install Docker, Minikube, and kubectl
 k8s_node.addService(rspec.Execute(
